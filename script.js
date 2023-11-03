f8a427c73cf32b99841334aadf9f5479aa6410a8
Колега-розробник <orions3171@ukr.net>
2023-11-03 16:09:44 +0200

    #Task2. Add users loading
    Also css-styles modified for better user interface

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(function(){
-    myArray = $('.copwr_button');
+    //myArray = $('.copwr_button');
     
+    // Отримати список всіх копірайтерів і додати їх в контейнер копірайтерів
     $.getJSON("https://jsonplaceholder.typicode.com/users",function(data){
+        $(data).each(function(index){
+            $('#users_container').append(`<button class="copwr_button" value="${data[index].id}">${data[index].name}</button>`);
+        })
     });
 });
